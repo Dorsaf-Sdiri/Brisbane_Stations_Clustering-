ddadd959bdb3cc7614d9eb8d166f345e8bda6d8e
Dorsaf Sdiri <sdiridorsaf4@gmail.com>
2019-04-14 17:35:22 +0100

    Change paths

--- src/main/scala/com/citybikes/clustering/Properties.scala
+++ src/main/scala/com/citybikes/clustering/Properties.scala
@@ -8,8 +8,8 @@ object Properties {
 	val SET_MASTER = "local"
   val NUM_CLUSTERS = 5
 	val NUM_SEEDS =1L
-	val INPUT_DATA = "/home/sdiri/Bureau/Brisbane_CityBike.json"
-	val OUTPUT_DIR = "/home/sdiri/Bureau/Output"
-	val MODEL_DIR = "/home/sdiri/Bureau/Output"
-  val OUTPUT_JSON= "/home/sdiri/Bureau/ClassifiedDF.json"
+	val INPUT_DATA = "Brisbane_Stations_Clustering-/Brisbane_CityBike.json"
+	val OUTPUT_DIR = "Brisbane_Stations_Clustering-/Output"
+	val MODEL_DIR = "Brisbane_Stations_Clustering-/Output"
+  val OUTPUT_JSON= "Brisbane_Stations_Clustering-/ClassifiedDF.json"
 	}
